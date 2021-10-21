bd863c949be97c182555a7536b28786c9babf0e2
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2021-10-21 16:43:20 -0300

    fazendo uma funçao com varios nomes e estilizando com estilos de css

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View ,Text,StyleSheet} from "react-native";
+import estilos from "./estilo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Meu primeiro App com react-Native!</Text>
-      <StatusBar style="auto" />
+const muitosEstilos =() =>{
+  return(
+    <View style={estilos.conteiner}>
+      <Text style={estilos.vermelho}>Meu Primeiro Teste com Vermelho-pequeno</Text>
+      <Text style={estilos.azulGrande}>Meu Segundo teste com a cor Azul-Grande</Text>
+      <Text style={estilos.AzulPequeno}>Meu Terceiro Teste com a cor Azul-Pequeno</Text>
+      <Text style={estilos.vermelhoGrande}>Meu Quarto Teste com a cor Vermelho-Grande</Text>
     </View>
+
   );
 }
+export default muitosEstilos;
+// toda vez em que fizer uma funcão desse jeito acima vc deve importar ela mesma como default
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+// agora vamos fazer os estilo para cada tag <Text> ou para todos
+// pode ser em um arquivo aqui mesmo ou em arquivo separado
+// vamos estilizar em arquivo separado
+
+
+
---
+++ estilo.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet} from "react-native";
+const estilos = StyleSheet.create({
+    conteiner:{
+      marginTop: 50,
+    },
+    azulGrande:{
+      color:'blue',
+      fontWeight:'bold',
+      fontSize:34,
+    
+    },
+    AzulPequeno:{
+      color:'blue'
+    },
+    vermelho:{
+      color: 'red'
+    
+    },
+    vermelhoGrande:{
+      color: 'red',
+      fontWeight:'bold',
+      fontSize:34
+    
+    }
+})
+export default estilos
